8effbe7b93392b83f625f94821f9dca235d691d9
UyiStocks <olanrewajuuwagboe@gmail.com>
2025-11-14 20:24:46 -0330

    Update employee.js

--- employee.js
+++ employee.js
@@ -6,20 +6,20 @@ const menuOverlay = document.getElementById('menuOverlay');
 openMenu.addEventListener('click', () => menuOverlay.classList.add('active'));
 closeMenu.addEventListener('click', () => menuOverlay.classList.remove('active'));
 
-// Template data (2 templates each)
+// Template data
 const templates = {
   resume: [
-    { img: 'resume1.png', id: 'resume1' },
-    { img: 'resume2.png', id: 'resume2' }
+    { img: 'template1.png', id: 'resume1' },
+    { img: 'template2.png', id: 'resume2' }
   ],
 
   'cover-letter': [
-    { img: 'cover1.png', id: 'cover1' },
-    { img: 'cover2.png', id: 'cover2' }
+    { img: 'template1.png', id: 'cover1' },
+    { img: 'template2.png', id: 'cover2' }
   ],
 
   'resignation-letter': [
-    { img: 'resign1.png', id: 'resign1' },
-    { img: 'resign2.png', id: 'resign2' }
+    { img: 'template1.png', id: 'resign1' },
+    { img: 'template2.png', id: 'resign2' }
   ]
 };
