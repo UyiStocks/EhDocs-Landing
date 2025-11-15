9e4c0f0a154cb7e292133b5d792d556c02ca9cc2
UyiStocks <olanrewajuuwagboe@gmail.com>
2025-11-15 20:02:43 -0330

    Delete contact.js

--- contact.js
+++
@@ -1,7 +0,0 @@
-// --- Fullscreen overlay menu ---
-const openMenu = document.getElementById('openMenu');
-const closeMenu = document.getElementById('closeMenu');
-const menuOverlay = document.getElementById('menuOverlay');
-
-openMenu.addEventListener('click', () => menuOverlay.classList.add('active'));
-closeMenu.addEventListener('click', () => menuOverlay.classList.remove('active'));
